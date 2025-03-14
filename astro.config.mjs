e77e5a0524cc996cdbf9d46b7a6afcff4cc20cd2
maxsteiger <max_steiger@outlook.com>
2025-03-14 22:23:36 +0100

    setting up deployment through github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://maxsteiger.github.io',
+  //base: 'bollwerk21-web',
   vite: {
     plugins: [tailwindcss()]
   }
