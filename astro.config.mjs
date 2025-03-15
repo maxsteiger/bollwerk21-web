8971c8e54f3abb16e9ed38b8d371363f2bd1f791
maxsteiger <max_steiger@outlook.com>
2025-03-15 13:07:50 +0100

    fix: starting addition of basePath to all links to fix deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://maxsteiger.github.io',
-  //base: 'bollwerk21-web',
+  base: 'bollwerk21-web',
   vite: {
     plugins: [tailwindcss()]
   }
